5a27e3e98f2724bbd35d8ee3f15b90b9d46df818
Agus2020 <aagustin067@gmail.com>
2023-08-12 17:13:53 -0300

    Agregando carrito

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/guitarras.module.css'
 import Layout from '../components/layout'
-export default function Producto({guitarra}){
-    const {nombre,descripcion,imagen,precio} = guitarra[0].attributes;
-    return(
+export default function Producto({guitarra,agregarCarrito}){
+  const [cantidad,setCantidad] = useState(0)  
+  const {nombre,descripcion,imagen,precio} = guitarra[0].attributes;
+  console.log(cantidad) 
+  const handleSubmit = e =>{
+    e.preventDefault()
+    if(cantidad < 1){
+      alert('cantidad no valida')
+      return
+    }
+    
+    //Construir un objeto
+
+    const guitarraSeleccionada = {
+      id: guitarra[0].id,
+      nombre:nombre,
+      imagen:imagen.data.attributes.url,
+      precio:precio,
+      cantidad:cantidad
+    }
+    // Pasando la informacion
+    agregarCarrito(guitarraSeleccionada)
+
+
+
+  } 
+  return(
         <Layout title={`Guitarra ${nombre}`}>
                 <div className={styles.guitarra}>
                     <Image src={imagen.data.attributes.url} width={600} height={400} alt='asdds'/>
@@ -11,6 +36,20 @@ export default function Producto({guitarra}){
                         <h3>{nombre}</h3>
                         <p className={styles.descripcion}>{descripcion}</p>
                         <p className={styles.precio}>${precio}</p>
+                        <form className={styles.formulario} onSubmit={handleSubmit}>
+                          <label htmlFor="cantidad">Cantidad:</label>
+                          <select id="cantidad" onChange={e => setCantidad(+e.target.value)}>
+                            <option value="0">Seleccione</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                          </select>
+
+                          <input type='submit' value='Agregar al carrito'/>
+
+                        </form>
                     </div>
                 
                 </div>
